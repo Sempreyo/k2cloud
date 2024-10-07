be30ebaae31c5a5e1c4f1db3da6872690f42dd8c
ArtZ <snickgem@gmail.com>
2024-10-07 03:41:16 +0300

    homepage desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 /* Libs */
+//= ./../../node_modules/jquery/dist/jquery.min.js
 //= ./../../node_modules/swiper/swiper-bundle.min.js
+//= ./libs/select2.min.js
+//= ./libs/hystmodal.min.js
 
 /* Common */
 //= ./partials/main.js
--- src/js/partials/swiper-init.js
+++ src/js/partials/swiper-init.js
@@ -1,22 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const heroCarousel = document.querySelectorAll(".hero__slider");
+	const solutionsCarousel = document.querySelectorAll(".solutions__slider");
 
-	if (heroCarousel.length > 0) {
-		heroCarousel.forEach(elem => {
-			const heroCarouselNext = elem.nextElementSibling.querySelector(".swiper-btn-next");
-			const heroCarouselPrev = elem.nextElementSibling.querySelector(".swiper-btn-prev");
+	if (solutionsCarousel.length > 0) {
+		solutionsCarousel.forEach(elem => {
+			const solutionsCarouselNext = elem.nextElementSibling.querySelector(".swiper-btn-next");
+			const solutionsCarouselPrev = elem.nextElementSibling.querySelector(".swiper-btn-prev");
 
 			const slider = new Swiper(elem, {
 				slidesPerView: 1,
 				spaceBetween: 10,
 				loop: true,
 				navigation: {
-					nextEl: heroCarouselNext,
-					prevEl: heroCarouselPrev,
+					nextEl: solutionsCarouselNext,
+					prevEl: solutionsCarouselPrev,
 				},
-				autoplay: {
-					delay: 5000,
-					pauseOnMouseEnter: true
+				pagination: {
+					el: ".solutions__pagination",
+					bulletActiveClass: "swiper-pagination__bullet--active",
+					clickable: true,
+					renderBullet: function (index, className) {
+						let bulletTpl = "";
+
+						switch (index) {
+							case 0:
+								bulletTpl = `<span class="swiper-pagination__bullet ${className}"><svg><use xlink:href="./images/sprite.svg#solutions1"></use></svg></span>`;
+								break;
+							case 1:
+								bulletTpl = `<span class="swiper-pagination__bullet ${className}"><svg><use xlink:href="./images/sprite.svg#solutions2"></use></svg></span>`;
+								break;
+							case 2:
+								bulletTpl = `<span class="swiper-pagination__bullet ${className}"><svg><use xlink:href="./images/sprite.svg#solutions3"></use></svg></span>`;
+								break;
+							case 3:
+								bulletTpl = `<span class="swiper-pagination__bullet ${className}"><svg><use xlink:href="./images/sprite.svg#solutions4"></use></svg></span>`;
+								break;
+							case 4:
+								bulletTpl = `<span class="swiper-pagination__bullet ${className}"><svg><use xlink:href="./images/sprite.svg#solutions5"></use></svg></span>`;
+								break;
+						}
+						return bulletTpl;
+					}
 				}
 			});
 		});
